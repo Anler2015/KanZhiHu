46d2716e73993d54cfec213e0c1a0ecb38c8af22
葛佳辉 <hippogjh7758258@qq.com>
2016-03-12 16:12:11 +0800

    编写自定义的request
    测试通过

--- app/src/main/java/com/gejiahui/kanzhihu/ui/MainActivity.java
+++ app/src/main/java/com/gejiahui/kanzhihu/ui/MainActivity.java
@@ -4,13 +4,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.gejiahui.kanzhihu.R;
 import com.gejiahui.kanzhihu.base.BaseActivity;
+import com.gejiahui.kanzhihu.model.Answer;
+import com.gejiahui.kanzhihu.net.Request4Answers;
 import com.orhanobut.logger.Logger;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class MainActivity extends BaseActivity {
@@ -21,7 +34,20 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        RequestQueue mQueue = Volley.newRequestQueue(this);
+        Request4Answers jsonObjectRequest = new Request4Answers("http://api.kanzhihu.com/getpostanswers/20150925/archive",
+                new Response.Listener<ArrayList<Answer>>() {
+                    @Override
+                    public void onResponse(ArrayList<Answer> response) {
+                        Logger.d(response.get(0).getAnswerUrl());
+                    }
+                }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
+            }
+        });
+        mQueue.add(jsonObjectRequest);
 
     }
 
